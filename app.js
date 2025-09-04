d2ba494e919a0a82ace24ce6ebcbc2c65d9cc867
Asad Noor <asadsipra2002@gmail.com>
2025-09-04 18:38:18 +0500

    redis app

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
 import Product from "./model.js";
+import { createClient } from "redis";
 const app = express();
+
+const redisClient = createClient({
+  url: "redis://127.0.0.1:6379",
+});
+
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/getProduct/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const cachedProduct = await redisClient.get(id);
+
+  if (cachedProduct) {
+    return res.json({ source: "Redis", product: JSON.parse(cachedProduct) });
+  }
+  const product = await Product.findById(id);
+
+  await redisClient.setEx(id, 60, JSON.stringify(product));
+  if (product) {
+    return res.json({ source: "Redis", product });
+  }
+  if (!product) {
+    return res.status(400).send("Product not found");
+  }
+  res.json({ source: "DB", product });
 });
 
 app.post("/addproduct", async (req, res) => {
